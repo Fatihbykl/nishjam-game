ca90a62fb490f503006a7870cde3d52a504c68b2
FantaLaTone <fantalatone@gmail.com>
2025-12-28 10:59:20 +0300

    menus

---
+++ Assets/Ardas/Scripts/Menu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+	public void StartGame() {
+		SceneManager.LoadScene("SampleScene");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
